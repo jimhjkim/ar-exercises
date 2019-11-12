7bb13642b7ebc59d45ed008a6a77a3182c786375
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-11 23:22:33 -0500

    exercise 5 complete: calculate basic stats related to annual revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total annual revenue: $#{Store.all.sum('annual_revenue')}"
+# puts "Average annual revenue: $#{Store.all.sum('annual_revenue') / Store.all.count}"
+puts "Average annual revenue: $#{Store.all.average('annual_revenue')}"
+
+puts "# of stores generating $1M or more in annual sales: #{Store.where('annual_revenue >= 1000000').size}"
