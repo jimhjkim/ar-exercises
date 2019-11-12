e849e878bf8304fa29bfc5c23fd658d5d9ca4692
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-12 00:59:15 -0500

    added before_create callback method for Employee class to insert a password

--- lib/employee.rb
+++ lib/employee.rb
@@ -5,4 +5,16 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
   validates :store_id, presence: true
+
+  before_create :password
+
+  private
+
+  def password(length = 8)
+    source = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a + ['_', '-', '.']
+    key = ''
+    length.times { key += source[rand(source.size)].to_s }
+    self.password = key
+  end
+
 end
