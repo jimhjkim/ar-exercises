7edadf243b2e47e4c309c2e4d331e9737e67036f
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-11 22:46:46 -0500

    exercise 2 completed: can find store by id and update its name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# update approach 1
+# @store1.name = 'Distillery'
+# @store1.save
+
+# update approach 2
+@store1.update(name: 'Distillery')
